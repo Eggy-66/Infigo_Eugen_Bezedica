d4a90ae434499431b2702513292b4714976ba407
eggy-66 <7385nmo@gmail.com>
2024-11-30 14:08:18 +0200

    updated database, added comment table

--- CMSPlus.Domain/Persistance/ApplicationDbContext.cs
+++ CMSPlus.Domain/Persistance/ApplicationDbContext.cs
@@ -15,6 +15,9 @@ public class ApplicationDbContext : IdentityDbContext
     {
     }
 
+        // DbSets for your entities
+    public DbSet<TopicEntity> Topics { get; set; } = null!;
+    public DbSet<CommentEntity> Comments { get; set; } = null!; 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
@@ -23,6 +26,13 @@ public class ApplicationDbContext : IdentityDbContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfiguration(new TopicEntityConfiguration());
+                // Configure relationship between TopicEntity and CommentEntity
+            builder.Entity<CommentEntity>()
+            .HasOne(c => c.Topic)
+            .WithMany(t => t.Comments)
+            .HasForeignKey(c => c.TopicId)
+            .OnDelete(DeleteBehavior.Cascade); // Cascade delete comments when a topic is deleted
+
         base.OnModelCreating(builder);
     }
 }
